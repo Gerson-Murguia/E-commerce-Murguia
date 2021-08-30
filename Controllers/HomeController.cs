6ee9157cb9b2779dfdec507bc9ddb72fb1c18ef2
Gerson-Murguia <aldmurguia1@gmail.com>
2021-08-29 22:35:29 -0500

    Agregado Marca Logica y Metodos del Home controller para enviar Json

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -53,6 +53,7 @@ namespace Proyecto05ciclo.Controllers
             return View();
         }
 
+        //CATEGORIA AJAX
         [HttpGet]
         public JsonResult ListarCategoria()
         {
@@ -60,5 +61,53 @@ namespace Proyecto05ciclo.Controllers
             oLista = CategoriaLogica.Instancia.Listar();
             return Json(new {data=oLista},JsonRequestBehavior.AllowGet);
         }
+
+        [HttpPost]
+        public JsonResult GuardarCategoria(Categoria oCategoria)
+        {
+            bool respuesta = false;
+            respuesta = (oCategoria.IdCategoria == 0)
+                ? CategoriaLogica.Instancia.Registrar(oCategoria)
+                : CategoriaLogica.Instancia.Modificar(oCategoria);
+            return Json(new {resultado=respuesta});
+        }
+
+        [HttpPost]
+        public JsonResult EliminarCategoria(int id)
+        {
+            bool respuesta = false;
+            respuesta = CategoriaLogica.Instancia.Eliminar(id);
+            return Json(new { resultado=respuesta});
+        }
+
+
+        //MARCA AJAX
+        [HttpGet]
+        public JsonResult ListarMarca()
+        {
+            List<Categoria> oLista=new List<Categoria>();
+            oLista = CategoriaLogica.Instancia.Listar();
+            return Json(new {data=oLista},JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarMarca(Marca oMarca)
+        {
+            bool respuesta = false;
+            respuesta = (oMarca.IdMarca == 0)
+                ? MarcaLogica.Instancia.Registrar(oMarca)
+                : MarcaLogica.Instancia.Modificar(oMarca);
+            return Json(new {resultado=respuesta});
+        }
+
+        [HttpPost]
+        public JsonResult EliminarMarca(int id)
+        {
+            bool respuesta = false;
+            respuesta = MarcaLogica.Instancia.Eliminar(id);
+            return Json(new { resultado=respuesta});
+        }
+
+        //PRODUCTO AJAX
     }
 }
---
+++ Logica/MarcaLogica.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Proyecto05ciclo.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Proyecto05ciclo.Logica
+{
+    public class MarcaLogica
+    {
+        private static MarcaLogica _instancia;
+
+        public MarcaLogica()
+        {
+        }
+
+        //SINGLETON
+        public static MarcaLogica Instancia
+        {
+            get
+            {
+                if (_instancia==null)
+                {
+                    _instancia=new MarcaLogica();
+                }
+                return _instancia;
+            }
+        }
+
+        public List<Marca> Listar()
+        {
+            List<Marca> oListaMarca=new List<Marca>();
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                SqlCommand cmd = new SqlCommand("sp_obtenerMarca", oConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    oConexion.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        oListaMarca.Add(new Marca()
+                        {
+                            IdMarca = Convert.ToInt32(dr["IdMarca"].ToString()),
+                            Descripcion = dr["Descripcion"].ToString(),
+                            Activo = Convert.ToBoolean(dr["Activo"].ToString())
+                        });
+                    }
+                    dr.Close();
+
+                    return oListaMarca;
+
+                }
+                catch (Exception ex)
+                {
+                    oListaMarca = null;
+                    return oListaMarca;
+                }
+            }
+        }
+
+        
+
+        public bool Registrar(Marca oMarca)
+        {
+            bool respuesta = true;
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarMarca", oConexion);
+                    cmd.Parameters.AddWithValue("Descripcion", oMarca.Descripcion);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+            }
+            return respuesta;
+        }
+
+        public bool Modificar(Marca oMarca)
+        {
+            bool respuesta = true;
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("sp_ModificarMarca", oConexion);
+                    cmd.Parameters.AddWithValue("IdMarca", oMarca.IdMarca);
+                    cmd.Parameters.AddWithValue("Descripcion", oMarca.Descripcion);
+                    cmd.Parameters.AddWithValue("Activo", oMarca.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+
+            }
+
+            return respuesta;
+
+        }
+
+        public bool Eliminar(int id)
+        {
+            bool respuesta = true;
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("delete from Marca where idMarca = @id", oConexion);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = true;
+
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+
+            }
+
+            return respuesta;
+        }
+    }
+}
