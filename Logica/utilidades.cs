bd3322d5a21152752c0b15d6e37714751ea4caa4
Gerson-Murguia <aldmurguia1@gmail.com>
2021-08-30 16:56:10 -0500

    Utilidad para obtener un string en base64 a partir de una ruta

---
+++ Logica/utilidades.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto05ciclo.Logica
+{
+    public class utilidades
+    {
+        //retorna un string base64 de la ruta especificada
+        public static string convertirBase64(string ruta)
+        {
+            byte[] bytes = File.ReadAllBytes(ruta);
+            string file = Convert.ToBase64String(bytes);
+            return file;
+        }
+    }
+}
